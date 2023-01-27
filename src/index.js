6be00a6c5640daad932b4e8088eb36233ccb409d
Maftuna <98867890+kifo3@users.noreply.github.com>
2023-01-27 20:49:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+
+const News = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+            <div>
+                <pre>{props.extra}</pre>
+                <p>{props.text}</p>
+            </div>
+            <div>
+                <h1>{props.header}</h1>
+            </div>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <News name='BBC News' extra='2000' text='Maftuna Boborakhimova' header='BigJob'/>
+        </div>
+    )}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +36,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
